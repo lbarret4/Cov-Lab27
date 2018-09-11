e3650ea2150df6b13f7cf1b7f1c9be79e13b60dc
lbarret4 <llewellynbar4@gmail.com>
2018-09-11 19:57:24 -0400

    Fixed formating and changed let to const

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-let path = require('path');
-let fs = require('fs');
+const path = require('path');
+const fs = require('fs');
 
-let chirps = [
+const chirps = [
     {
-        time:new Date(2018, 7, 27, 7, 0).toString(),
-        user:'Charles',
-        content:'I ate the best pizza I ever had!',
-    
+        time: new Date(2018, 7, 27, 7, 0).toString(),
+        user: 'Charles',
+        content: 'I ate the best pizza I ever had!',
+
     },
     {
         time: new Date(2018, 7, 28, 4, 0).toString(),
@@ -14,29 +14,29 @@ let chirps = [
         content: 'Who send greeting cards anymore?',
     },
     {
-        time:new Date(2018, 7, 25, 17, 0).toString(),
-        user:'Kim',
-        content:'I am going to be the card dealer tonight! ',
+        time: new Date(2018, 7, 25, 17, 0).toString(),
+        user: 'Kim',
+        content: 'I am going to be the card dealer tonight! ',
     },
     {
         time: new Date(2018, 7, 29, 3, 30).toString(),
         user: 'Amanda',
-        content:'Music: Radio, CDS, MP3s, Streaming',
+        content: 'Music: Radio, CDS, MP3s, Streaming',
     },
     {
-        time:new Date(2018, 7, 23, 18,15).toString(),
+        time: new Date(2018, 7, 23, 18, 15).toString(),
         user: 'Kenji',
-        content:'I left my hair brush at home.',
+        content: 'I left my hair brush at home.',
     },
-] ;
+];
 
-let dataPath =path.join(__dirname,'chirps.json');
-fs.writeFile(dataPath,JSON.stringify(chirps),(err) => {
-    if(err) throw err;
+const dataPath = path.join(__dirname, 'chirps.json');
+fs.writeFile(dataPath, JSON.stringify(chirps), (err) => {
+    if (err) throw err;
 });
 
-fs.readFile(dataPath,(err,data) => {
-    if(err) throw err;
+fs.readFile(dataPath, (err, data) => {
+    if (err) throw err;
     console.log(JSON.parse(data));
 })
 
